de83ee96066a9cd7095def43a0609bc8e3dd0add
lixia <873575364@qq.com>
2016-07-07 14:50:44 +0800

    revise louvain parameter

--- use_louvain.py
+++ use_louvain.py
@@ -17,7 +17,7 @@ def read_file(file_name):
 
 
 def write_file(G, file_name):
-	partition = cm.best_partition(G, resolution=10.0)
+	partition = cm.best_partition(G)
 
 	with open(file_name, 'w') as fp:
 		for node_id, c_id in partition.items():
@@ -25,23 +25,20 @@ def write_file(G, file_name):
 
 	return
 
-	community_num = len(set(partition.values()))
-	hist, bin_edges = np.histogram(partition.values(), bins=community_num)
-	indexes = np.arange(community_num)[hist>50]
-	index_map = dict(zip(indexes, range(len(indexes))))
-	print len(indexes)
+	community_num = max(partition.values())
+	hist, bin_edges = np.histogram(partition.values(), bins=range(community_num))
+	indexes = bin_edges[hist>5]
+	index_set = set(indexes)
 
 	with open(file_name, 'w') as fp:
 		for node_id, c_id in partition.items():
-			try:
-				fp.write(str(2**index_map[c_id]) + '\n')
-			except KeyError, e:
-				fp.write('0\n')
+			if c_id in index_set:
+				fp.write(str(node_id) + ',' + str(c_id) + '\n')
 
 
 def main():
-	read_file_name = 'inventor_graph.txt'
-	write_file_name = 'inventor_community2.txt'
+	read_file_name = 'dblp_graph.txt'
+	write_file_name = 'dblp_community2.txt'
 	G = read_file(read_file_name)
 	write_file(G, write_file_name)
 
